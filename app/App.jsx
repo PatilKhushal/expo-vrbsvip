08463180c2a4d03f54e9a9e2526706d201641ece
PatilKhushal <patilkhushalp2803@gmail.com>
2025-02-26 14:16:19 +0530

    done till nativewind setup

---
+++ app/App.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import '../global.css'
+
+const App = () => {
+  return (
+    <View className="h-full w-full bg-black">
+      <Text className="text-green-500">App</Text>
+    </View>
+  )
+}
+
+export default App
